a368d52dbb7d35a9935dede523a5b666f87f1e0a
Vivek <vivek.7266@gmail.com>
2019-02-05 02:35:09 -0500

    refactoring

--- src/baseline.py
+++ src/baseline.py
@@ -1,52 +1,8 @@
-# Andreas Mauczka - dictionary based algorithm
-# Tracing Your Maintenance Work – A Cross-Project Validation of an Automated Classification Dictionary for Commit Messages
-# https://dl.acm.org/citation.cfm?id=2259307
-import glob
-import os
-import re
-import string
-
-from nltk import RegexpTokenizer
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-
-import pandas as pd
-import numpy as np
-
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-
-from src.config import Metrics
-
-corrective_seed = {"fix", "bug", "problem", "incorrect", "correct", "error", "fixup", "fail"}
-adaptive_seed = {"new", "change", "patch", "add", "modify", "update"}
-perfective_seed = {"style", "move", "removal", "cleanup", "unneeded", "rework"}
-
-mauckza_corrective_seed = {'active', 'against', 'already', 'bad', 'block', 'bug', 'build', 'call', 'case', 'catch',
-                           'cause', 'character', 'compile', 'correctly', 'create', 'different', 'dump', 'error',
-                           'except', 'exist', 'explicitly', 'fail', 'failure', 'fast', 'fix', 'format', 'good', 'hack',
-                           'hard', 'help', 'init', 'instead', 'introduce', 'issue', 'lock', 'log', 'logic', 'look',
-                           'merge', 'miss', 'null', 'oops', 'operation', 'operations', 'pass', 'previous', 'previously',
-                           'probably', 'problem', 'properly', 'random', 'recent', 'request', 'reset', 'review', 'run',
-                           'safe', 'set', 'similar', 'simplify', 'special', 'test', 'think', 'try', 'turn', 'valid',
-                           'wait', 'warn', 'warning', 'wrong'}
-mauckza_adaptive_seed = {'active', 'add', 'additional', 'against', 'already', 'appropriate', 'available', 'bad',
-                         'behavior', 'block', 'build', 'call', 'case', 'catch', 'change', 'character', 'compatibility',
-                         'compile', 'config', 'configuration', 'context', 'correctly', 'create', 'currently', 'default',
-                         'different', 'documentation', 'dump', 'easier', 'except', 'exist', 'explicitly', 'fail',
-                         'fast', 'feature', 'format', 'future', 'good', 'hack', 'hard', 'header', 'help', 'include',
-                         'information', 'init', 'inline', 'install', 'instead', 'internal', 'introduce', 'issue',
-                         'lock', 'log', 'logic', 'look', 'merge', 'method', 'necessary', 'new ', 'old', 'operation',
-                         'operations', 'pass', 'patch', 'previous', 'previously', 'probably', 'properly',
-                         'protocol provide', 'random', 'recent', 'release', 'replace ,request', 'require', 'reset',
-                         'review', 'run', 'safe', 'security', 'set', 'similar', 'simple', 'simplify', 'special',
-                         'structure', 'switch', 'test', 'text', 'think', 'trunk', 'try', 'turn', 'useful', 'user',
-                         'valid', 'version', 'wait'}
-mauckza_perfective_seed = {'cleanup', 'consistent', 'declaration', 'definition', 'header', 'include', 'inline', 'move',
-                           'prototype', 'removal', 'static', 'style', 'unused', 'variable', 'warning', 'whitespace'}
-mauckza_blacklist_seed = {'cvs2svn', 'cvs', 'svn'}
-
 """
+Andreas Mauczka - dictionary based algorithm
+Tracing Your Maintenance Work – A Cross-Project Validation of an Automated Classification Dictionary for Commit Messages
+https://dl.acm.org/citation.cfm?id=2259307
+
 1. Classify the commit using the “seed” dictionary
 2. If the total percentage of classified commits is greater than 80%, EXIT
 3. Count the appearances of all words in the commit messages of the nonclassified
@@ -96,6 +52,51 @@ Blacklist: cvs2svn, cvs, svn
 
 
 """
+
+import glob
+import os
+import re
+import string
+
+from nltk import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+from src.config import Metrics
+
+corrective_seed = {"fix", "bug", "problem", "incorrect", "correct", "error", "fixup", "fail"}
+adaptive_seed = {"new", "change", "patch", "add", "modify", "update"}
+perfective_seed = {"style", "move", "removal", "cleanup", "unneeded", "rework"}
+
+mauckza_corrective_seed = {'active', 'against', 'already', 'bad', 'block', 'bug', 'build', 'call', 'case', 'catch',
+                           'cause', 'character', 'compile', 'correctly', 'create', 'different', 'dump', 'error',
+                           'except', 'exist', 'explicitly', 'fail', 'failure', 'fast', 'fix', 'format', 'good', 'hack',
+                           'hard', 'help', 'init', 'instead', 'introduce', 'issue', 'lock', 'log', 'logic', 'look',
+                           'merge', 'miss', 'null', 'oops', 'operation', 'operations', 'pass', 'previous', 'previously',
+                           'probably', 'problem', 'properly', 'random', 'recent', 'request', 'reset', 'review', 'run',
+                           'safe', 'set', 'similar', 'simplify', 'special', 'test', 'think', 'try', 'turn', 'valid',
+                           'wait', 'warn', 'warning', 'wrong'}
+mauckza_adaptive_seed = {'active', 'add', 'additional', 'against', 'already', 'appropriate', 'available', 'bad',
+                         'behavior', 'block', 'build', 'call', 'case', 'catch', 'change', 'character', 'compatibility',
+                         'compile', 'config', 'configuration', 'context', 'correctly', 'create', 'currently', 'default',
+                         'different', 'documentation', 'dump', 'easier', 'except', 'exist', 'explicitly', 'fail',
+                         'fast', 'feature', 'format', 'future', 'good', 'hack', 'hard', 'header', 'help', 'include',
+                         'information', 'init', 'inline', 'install', 'instead', 'internal', 'introduce', 'issue',
+                         'lock', 'log', 'logic', 'look', 'merge', 'method', 'necessary', 'new ', 'old', 'operation',
+                         'operations', 'pass', 'patch', 'previous', 'previously', 'probably', 'properly',
+                         'protocol provide', 'random', 'recent', 'release', 'replace ,request', 'require', 'reset',
+                         'review', 'run', 'safe', 'security', 'set', 'similar', 'simple', 'simplify', 'special',
+                         'structure', 'switch', 'test', 'text', 'think', 'trunk', 'try', 'turn', 'useful', 'user',
+                         'valid', 'version', 'wait'}
+mauckza_perfective_seed = {'cleanup', 'consistent', 'declaration', 'definition', 'header', 'include', 'inline', 'move',
+                           'prototype', 'removal', 'static', 'style', 'unused', 'variable', 'warning', 'whitespace'}
+mauckza_blacklist_seed = {'cvs2svn', 'cvs', 'svn'}
 
 lemmatizer = WordNetLemmatizer()
 rt = RegexpTokenizer(r'[^\W_]+|[^\W_\s]+')
